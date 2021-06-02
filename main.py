e200eb4541a816ab2c032d889091dc13c5dab6f3
5pread <68393055+5pread@users.noreply.github.com>
2021-06-02 23:35:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import pybase64
+
+def EncodeText():
+    text = input('Text to encode >>')
+    text = text.encode()
+    encoded_text = pybase64.standard_b64encode(text)
+    encoded_text = str(encoded_text)
+    encoded_text = encoded_text[2:][:-1]
+    print(f'Your encoded text >> {encoded_text}')
+
+def DecodeText():
+    string = input('Text to decode >>')
+    decoded_text = pybase64.standard_b64decode(string)
+    decoded_text = str(decoded_text)[2:][:-1]
+    print(f'Your decoded text >> {decoded_text}')
+
+DecodeText()
